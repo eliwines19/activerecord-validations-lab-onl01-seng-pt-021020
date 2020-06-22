eb4133f7e46d77c9d39de039b3ee8c684017648b
Elijah wines <elijahwines84@yahoo.com>
2020-06-22 01:48:48 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250}
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-  validates :not_clickbait
+  validate :not_clickbait
 
   ARR = [
     /Won't Believe/,
