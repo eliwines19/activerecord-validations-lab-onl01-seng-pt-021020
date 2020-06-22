1036248e757d8727996a2d342cee59f433069dd6
Elijah wines <elijahwines84@yahoo.com>
2020-06-22 01:48:06 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
 
   validates :title, presence: true
-  validates :content, length { minimum: 250}
-  validates :summary, length { maximum: 250}
+  validates :content, length: { minimum: 250}
+  validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
   validates :not_clickbait
 
